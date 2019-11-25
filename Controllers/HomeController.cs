128bcc4ff1b1568f83071dcb1b5bf7c8eb1ce257
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-11-25 14:43:13 +0700

    menambahkan controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleASPCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("From ASP MVC Core !");
+        }
+
+        public IActionResult About()
+        {
+            return Content("Hello from from About");
+        }
+    }
+}
