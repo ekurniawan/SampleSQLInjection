28b34a4c696cbb9506045b33aa66c4335ef43ad8
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-11-27 13:54:44 +0700

    menambahkan menu

--- Models/Restaurant.cs
+++ Models/Restaurant.cs
@@ -7,6 +7,7 @@ namespace SampleASPCore.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage ="Field Name harus diisi")]
+        //[RegularExpression(@"^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$",ErrorMessage ="Format email tidak sesuai")]
         public string Name { get; set; }
     }
 }
