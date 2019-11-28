df0b4569e132e97ef5855d802643e18e67bd3973
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-11-28 14:10:08 +0700

    menambahkan migrasi

--- Startup.cs
+++ Startup.cs
@@ -5,19 +5,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SampleASPCore.Data;
 using SampleASPCore.Services;
 
 namespace SampleASPCore
 {
     public class Startup
     {
+        private IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    _config.GetConnectionString("DefaultConnection")));
+
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddSingleton<IGreeter, Greeter>();
